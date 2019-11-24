138cabc5b56909f3fd2c54706fe24d434621bac7
tekkenthuuug <sa.soto317@gmail.com>
2019-11-24 16:01:33 +0100

    Updates

---
+++ precache-manifest.bf83dfb3c749727c019f121e49bbbcfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e12fe83848d3c1ea0b105cdd8faf6d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8318a26ec0a2f8b681e5",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "682b3952e5c56c09c7ab",
+    "url": "/robofriends/static/css/main.e13079c1.chunk.css"
+  },
+  {
+    "revision": "8318a26ec0a2f8b681e5",
+    "url": "/robofriends/static/js/2.2b47b015.chunk.js"
+  },
+  {
+    "revision": "682b3952e5c56c09c7ab",
+    "url": "/robofriends/static/js/main.e8130129.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
